21a2a8c4c1ec8ff5b301d9fe3c2b42aa8fbc48c8
Kyle0381 <bks0381@gmail.com>
2025-12-18 08:37:20 +0800

    fix User from supabaseClient

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,10 @@
 // lib/supabaseClient.ts
-import { createClient, SupabaseClient } from "@supabase/supabase-js";
+import { createClient, type SupabaseClient, type Session, type User as SupabaseUser } from "@supabase/supabase-js";
 
 const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
+
+// Export the type directly
+export type { SupabaseUser as User, Session };
